64146e1718b0e66efa3006229177fafffde2bb71
grayshark_ <basov2764@gmail.com>
2026-06-09 20:16:40 +1100

    fix

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ async def send_main_menu(target, user_id, username=None):
             await target.message.delete()
             await target.message.answer_photo(photo=photo, caption=text, reply_markup=buttons, parse_mode='HTML')
     else:
-        await target.answer('​', reply_markup=_main_menu_kbd)
+        await target.answer('⠀', reply_markup=_main_menu_kbd)
         await target.answer_photo(photo=photo, caption=text, reply_markup=buttons, parse_mode='HTML')
 
 
